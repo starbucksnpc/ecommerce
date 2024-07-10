e5a48e47d61cc203f79ad05d62d535f862118d58
Sol Nam <cbkpwr@gmail.com>
2024-07-10 09:28:17 -0500

    Gain access to an admin dashboard by providing a username and pw.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,15 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authenticate_user! #모든 사용자 인증 필요
+  before_action :authorize_admin! #관리자 권한 확인
+
   def index
+  end
+
+  private
+
+  def authorize_admin!
+    unless current_user.admin?
+      redirect_to(root_path, alert: 'Access restricted to administrators only.')
+    end
   end
 end
